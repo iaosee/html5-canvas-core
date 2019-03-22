5990cd8702df4f561753b3e874e2fe8b0172c8b4
iaosee <iaosee@outlook.com>
2019-03-22 16:26:35 +0800

    add Random class, update Demo.03

--- src/script/Demo.03.ts
+++ src/script/Demo.03.ts
@@ -1,13 +1,20 @@
 import DemoBase from './DemoBase';
+import Random from './tools/Random';
 import { Point, Circle } from './declare';
 
 export default class Demo extends DemoBase {
 
-  public circles: Array<Circle> = [];
+  private circles: Array<Circle> = [];
+  private random: Random = Random.init(-5, 5);
 
   public constructor(public canvas: HTMLCanvasElement) {
     super(canvas);
-    this.createCircle();
+    const pos: Point = {
+      x: this.centerX,
+      y: this.centerY
+    }
+    this.createCircle(pos, 300)
+        .listenEvents();
   }
 
   public static init(canvas: HTMLCanvasElement): Demo {
@@ -23,17 +30,22 @@ export default class Demo extends DemoBase {
                .startPlay();
   }
 
-  private createCircle(quantity: number = 100) {
+  private createCircle(
+    position: Point,
+    quantity: number = 100,
+    clean: boolean = false
+  ) {
 
+    clean && this.circles.splice(0, this.circles.length);
     for ( let i = 0; i < quantity; i++ ) {
       let point: Point = {
-        x: this.centerX,
-        y: this.centerY
+        x: position.x || this.centerX,
+        y: position.y || this.centerY
       };
       this.circles.push({
         position: point,
-        velocityX: Math.random() * 5,
-        velocityY: Math.random() * -5,
+        velocityX: Math.random() * (this.random.range(-5, 5).getOne() || 5),
+        velocityY: Math.random() * (this.random.range(-5, 5).getOne() || 5),
         radius: Math.random() * 50,
         color: this.randomRgba(),
       });
@@ -71,7 +83,6 @@ export default class Demo extends DemoBase {
 
   private drawCircles() {
     const { context } = this;
-
     this.circles.forEach((circle) => {
       context.beginPath();
       context.arc(
@@ -82,8 +93,12 @@ export default class Demo extends DemoBase {
         Math.PI * 2,
         false
       );
+
+      // context.lineWidth = 1.5;
       context.fillStyle = circle.color;
       context.fill();
+      // context.strokeStyle = circle.color;
+      // context.stroke();
 
       this.updatePosition(circle);
    });
@@ -108,4 +123,15 @@ export default class Demo extends DemoBase {
     return this;
   }
 
+  private listenEvents() {
+    const { canvas } = this;
+
+    canvas.addEventListener('click', (e: MouseEvent) => {
+      const coordinate: Point = this.CoordinateTransformation(e.clientX, e.clientY);
+
+      this.createCircle(coordinate, 100, false);
+
+    }, false);
+
+  }
 }
---
+++ src/script/tools/Random.ts
@@ -0,0 +1,70 @@
+
+export default class Random {
+
+  private minValue: number;
+  private maxValue: number;
+
+  public constructor(min?: number, max?: number) {
+    if ( !(this instanceof Random) )  {
+      return new Random(min, max);
+    }
+
+    this.minValue = Number.isFinite(min) ? min : this.minValue;
+    this.maxValue = Number.isFinite(max) ? max : this.maxValue;
+  }
+
+  public static init(min?: number, max?: number): Random {
+    if ( min > max ) {
+      throw new Error(`min(${min}) greater than max(${max})`);
+    }
+    return new Random(min, max);
+  }
+
+  public min(min: number) {
+    this.minValue = Number.isFinite(min) ? min : this.minValue;
+    return this;
+  }
+
+  public max(max: number) {
+    if ( max < this.minValue ) {
+      throw new Error(`max(${max}) less than min(${this.minValue})`);
+    }
+    this.maxValue = Number.isFinite(max) ? max : this.maxValue;
+    return this;
+  }
+
+  public range(min: number, max: number) {
+    if ( min > max ) {
+      throw new Error(`min(${min}) greater than max(${max})`);
+    }
+    return this.min(min).max(max);
+  }
+
+  public random() {
+    return  Math.round(Math.random() * (this.maxValue - this.minValue)) + this.minValue;
+  }
+
+  public getOne() {
+    return this.random();
+  }
+
+  public getGroup(count: number) {
+    const numList: Array<number> = [];
+
+    if ( count > (this.maxValue - this.minValue) + 1 ) {
+      throw new Error(`${this.minValue} ~ ${this.maxValue} 范围不能生成 ${count} 个值`);
+    }
+
+    let randNum = this.getOne();
+    while ( numList.length < count ) {
+      if ( numList.includes(randNum) ) {
+        randNum = this.getOne();
+      } else {
+        numList.push(randNum);
+      }
+    }
+
+    return numList;
+  }
+
+}
