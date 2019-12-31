7108164997f2485a406028bf9337de0d632ec961
iaosee <iaosee@outlook.com>
2019-12-31 18:37:14 +0800

    :beers: Update Demo.20

--- src/script/Demo.20.ts
+++ src/script/Demo.20.ts
@@ -1,5 +1,8 @@
 import { BaseDemo } from './BaseDemo';
 
+/**
+ * @description
+ */
 export class Demo extends BaseDemo {
   public constructor(public canvas: HTMLCanvasElement) {
     super(canvas);
@@ -22,15 +25,44 @@ export class Demo extends BaseDemo {
     context.stroke();
     // context.fill();
 
-    return this.drawRoundedRect(this.randomRgba(), this.randomRgba(), 50, 40, 100, 100, 10).drawRoundedRect(
-      this.randomRgba(),
-      this.randomRgba(),
-      this.centerX,
-      50,
-      300,
-      300,
-      20
-    );
+    return this.drawCircularBead()
+      .drawRoundedRect(this.randomRgba(), this.randomRgba(), 200, 200, 100, 100, 10)
+      .drawRoundedRect(this.randomRgba(), this.randomRgba(), this.centerX, 50, 300, 300, 20);
+  }
+
+  public drawCircularBead() {
+    const { context } = this;
+
+    context.setLineDash([]);
+    context.beginPath();
+    context.moveTo(150, 20);
+    context.arcTo(150, 100, 50, 20, 30);
+    context.strokeStyle = 'blue';
+    context.stroke();
+
+    context.fillStyle = 'blue';
+    // base point
+    context.fillRect(150, 20, 10, 10);
+    context.fillText('A', 160, 20);
+
+    context.fillStyle = 'red';
+    // control point one
+    context.fillRect(150, 100, 10, 10);
+    context.fillText('B', 160, 120);
+    // control point two
+    context.fillRect(50, 20, 10, 10);
+    context.fillText('C', 40, 40);
+
+    context.setLineDash([5, 5]);
+    context.moveTo(150, 20);
+    context.lineTo(150, 100);
+    context.lineTo(50, 20);
+    context.stroke();
+    context.beginPath();
+    context.arc(120, 38, 30, 0, 2 * Math.PI);
+    context.stroke();
+
+    return this;
   }
 
   public roundedRect(cornerX: number, cornerY: number, width: number, height: number, cornerRadius: number) {
