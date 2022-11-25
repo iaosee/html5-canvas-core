5d21b7ff67b41b0c2aea0e33a2b33a3dbf1f6779
iaosee <iaosee@outlook.com>
2022-11-25 15:31:15 +0800

    :art: update Demo.61, use MinimumTranslationVector

--- src/script/demo/Demo.61.ts
+++ src/script/demo/Demo.61.ts
@@ -2,11 +2,13 @@ import { GUI } from 'lil-gui';
 import { BaseDemo } from '../base/BaseDemo';
 import { Random } from '../tools/Random';
 import { Point } from '../geometry/Point';
+import { Vector } from '../geometry/Vector';
 import { Shape } from '../geometry/Shape';
 import { Circle } from '../geometry/Circle';
 import { Polygon } from '../geometry/Polygon';
 import { CircleImage } from '../geometry/CircleImage';
 import { RandomConvexPolygon } from '../geometry/RandomConvexPolygon';
+import { MinimumTranslationVector } from '../geometry/MinimumTranslationVector';
 
 import golfball from '../../../asset/images/golfball.png';
 
@@ -17,11 +19,16 @@ export class Demo extends BaseDemo {
   public name: string = '碰撞检测 — 最小平移向量';
 
   public shapes: Shape[] = [];
-  public shapeBeingDragged: Shape;
 
-  private mousedownPos = new Point(0, 0);
-  private mousemovePos = new Point(0, 0);
-  private randomPolygon = new RandomConvexPolygon({
+  public lastTime = 0;
+  public isStuck: boolean = false;
+  public shapeMoving: Shape;
+  public velocity = { x: 300, y: 200 };
+  public lastVelocity = { x: 300, y: 200 };
+
+  public mousedownPos = new Point(0, 0);
+  public mousemovePos = new Point(0, 0);
+  public randomPolygon = new RandomConvexPolygon({
     maxWidth: 200,
     maxHeight: 200,
   });
@@ -53,16 +60,28 @@ export class Demo extends BaseDemo {
     this.gui = new GUI();
     const { gui } = this;
 
-    gui.add(config, 'boundingBox').onFinishChange((value: string) => this.draw());
+    gui.add(config, 'boundingBox').onFinishChange((value: string) => this.drawScene());
     return this;
   }
 
-  public start() {
-    return this.draw();
-  }
+  public draw(timestamp: number = 0) {
+    const { shapeMoving, velocity } = this;
 
-  public draw() {
-    return this.clearScreen().drawGrid().drawScene();
+    if (!this.lastTime) {
+      this.lastTime = timestamp;
+    }
+
+    if (shapeMoving) {
+      const elapsedTime = timestamp - this.lastTime;
+      const dx = velocity.x * (elapsedTime / 1000);
+      const dy = velocity.y * (elapsedTime / 1000);
+      shapeMoving.move(dx, dy);
+    }
+
+    this.drawScene();
+
+    this.lastTime = timestamp;
+    return this;
   }
 
   public initShapes() {
@@ -81,11 +100,11 @@ export class Demo extends BaseDemo {
     }
 
     for (let i = 0; i < 2; i++) {
-      for (let j = 0; j < 5; j++) {
+      for (let j = 0; j < 3; j++) {
         const points = this.randomPolygon.getConvex(Random.init(4, 10).random());
         const polygon = new Polygon();
         polygon.setPoints(points);
-        polygon.move(j * 200, (i + 1) * 200);
+        polygon.move(j * 300, (i + 1) * 300);
         polygon.name = `Polygon ${i}-${j}`;
         polygon.strokeStyle = this.randomRgba();
         polygon.fillStyle = this.randomRgba();
@@ -93,8 +112,8 @@ export class Demo extends BaseDemo {
       }
     }
 
-    this.shapes.push(new Circle({ name: 'circle 1', x: 100, y: 50, radius: 30 })),
-      this.shapes.push(new Circle({ name: 'circle 2', x: 250, y: 50, radius: 50 }));
+    this.shapes.push(new Circle({ name: 'circle 1', x: 100, y: 50, radius: 30 }));
+    this.shapes.push(new Circle({ name: 'circle 2', x: 250, y: 50, radius: 50 }));
 
     this.shapes.push(
       new CircleImage({
@@ -120,84 +139,109 @@ export class Demo extends BaseDemo {
         context.strokeRect(rect.x, rect.y, rect.width, rect.height);
       }
     });
+
+    return this;
   }
 
   public drawScene() {
     const { context } = this;
 
-    this.drawShapes();
+    this.clearScreen().drawGrid().drawShapes().detectCollisions();
 
     return this;
   }
 
   public detectCollisions() {
-    const { context, shapes, shapeBeingDragged } = this;
+    const { context, shapes, shapeMoving, velocity } = this;
     let textY = 30;
 
-    if (!shapeBeingDragged) {
-      return;
+    if (!shapeMoving) {
+      return this;
     }
 
     context.save();
     context.font = '20px Palatino';
     shapes.forEach((shape) => {
-      if (shape === shapeBeingDragged) {
+      if (shape === shapeMoving) {
         return;
       }
-      const mtv = shapeBeingDragged.collidesMTVWith(shape);
-      if (mtv.axis) {
+      const mtv = shapeMoving.collidesMTVWith(shape);
+      if (mtv.axis || mtv.overlap) {
         // collided
         context.lineWidth = 10;
         context.fillStyle = 'red';
         context.strokeStyle = 'red';
-        context.fillText(`${shapeBeingDragged.name} Collision with ${shape.name}`, 20, textY);
+        context.fillText(`${shapeMoving.name} Collision with ${shape.name}`, 20, textY);
         context.strokeRect(0, 0, this.width, this.height);
         textY += 40;
+        !this.isStuck && this.stick(mtv);
       }
     });
     context.restore();
 
+    const bBox = shapeMoving?.getBoundingBox();
+    if (bBox.x + bBox.width > this.width || bBox.x < 0) {
+      velocity.x = -velocity.x;
+    }
+    if (bBox.y + bBox.height > this.height || bBox.y < 0) {
+      velocity.y = -velocity.y;
+    }
+
     return this;
   }
 
+  public stick(mtv: MinimumTranslationVector) {
+    const { shapeMoving, velocity, lastVelocity } = this;
+
+    if (!mtv.axis) {
+      // The object that's moving is a circle.
+      const point = new Point();
+      const velocityMagnitude = Math.sqrt(Math.pow(velocity.x, 2) + Math.pow(velocity.y, 2));
+      // Point the mtv axis in the direction of the circle's velocity.
+      point.x = velocity.x / velocityMagnitude;
+      point.y = velocity.y / velocityMagnitude;
+      mtv.axis = new Vector(point.x, point.y);
+    }
+
+    let dx = mtv.axis.x * (mtv.overlap + 2);
+    let dy = mtv.axis.y * (mtv.overlap + 2);
+
+    if ((dx < 0 && velocity.x < 0) || (dx > 0 && velocity.x > 0)) {
+      dx = -dx;
+    }
+
+    if ((dy < 0 && velocity.y < 0) || (dy > 0 && velocity.y > 0)) {
+      dy = -dy;
+    }
+
+    setTimeout(() => {
+      shapeMoving.move(dx, dy);
+    }, 500);
+
+    lastVelocity.x = velocity.x;
+    lastVelocity.y = velocity.y;
+    velocity.x = velocity.y = 0;
+
+    this.isStuck = true;
+  }
+
   public listenEvents() {
-    const { canvas, context, dpr, mousedownPos, mousemovePos, shapes } = this;
+    const { canvas, context, dpr, shapes, velocity, lastVelocity } = this;
 
     canvas.addEventListener('mousedown', (e: MouseEvent) => {
       const location = this.coordinateTransformation(e.clientX, e.clientY);
 
+      velocity.x = lastVelocity.x;
+      velocity.y = lastVelocity.y;
+
+      this.isStuck = false;
+      this.shapeMoving = undefined;
+
       shapes.forEach((shape) => {
         if (shape.isPointInPath(context, location.x * dpr, location.y * dpr)) {
-          this.shapeBeingDragged = shape;
-          mousedownPos.x = location.x;
-          mousedownPos.y = location.y;
-          mousemovePos.x = location.x;
-          mousemovePos.y = location.y;
+          this.shapeMoving = shape;
         }
       });
-    });
-    canvas.addEventListener('mousemove', (e: MouseEvent) => {
-      const location = this.coordinateTransformation(e.clientX, e.clientY);
-      if (!this.shapeBeingDragged) {
-        return;
-      }
-
-      const dragVector = {
-        x: location.x - mousemovePos.x,
-        y: location.y - mousemovePos.y,
-      };
-
-      this.shapeBeingDragged?.move(dragVector.x, dragVector.y);
-
-      mousemovePos.x = location.x;
-      mousemovePos.y = location.y;
-
-      this.draw();
-      this.detectCollisions();
-    });
-    canvas.addEventListener('mouseup', (e: MouseEvent) => {
-      this.shapeBeingDragged = null;
-      this.stick(mtv);
     });
 
     return this;
