5172b8606b9fee24b8698304fcd14f82a425db84
iaosee <iaosee@outlook.com>
2025-05-08 01:07:00 +0800

    Updates

---
+++ assets/Demo.00-BTI1ujAF.js
@@ -0,0 +1,1 @@
+import{B as r}from"./BaseDemo-CScLeyCm.js";class e extends r{constructor(t){super(t)}static init(t){return new e(t)}}export{e as Demo};
--- assets/Demo.00-DgkeWeiE.js
+++
@@ -1,1 +0,0 @@
-import{B as e}from"./BaseDemo-DB1y7toB.js";class s extends e{constructor(t){super(t),this.canvas=t}static init(t){return new s(t)}}export{s as Demo};
